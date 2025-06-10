b406f026876df458657d028270cfa2830998c118
aliqulamaliyev <aliqulamaliyev@gmail.com>
2025-06-10 12:05:23 +0000

    co,pletes  task

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
-
+/**
+ * main - prints with proper grammar ,but the outcome is a piece of art starting by a new line  
+ *
+ * Return: Always (succesful)
+ *
+ */
 int main(void)
 {
-    printf("with proper grammar, but the outcome is a piece of art,");
-    return 0;
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+    return (0);
 
