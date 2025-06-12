eb0308c818caf0f0276322476ac34a5074bce04c
aliqulamaliyev <aliqulamaliyev@gmail.com>
2025-06-12 09:36:23 +0000

    comlpetes the givrn task

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,27 +1,24 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/***
- * main - Entry point of the program
- *
+/**
+ * main - entyr
  * Description: This program generates a random number,
  * calculates its last digit, and checks if the last digit
  * is greater than 5, equal to 0, or less than 6 but not 0.
  * It then prints a corresponding message based on the last digit.
  *
  * Return: Always 0 (Success)
- *
- *
- *
  */
 
 int main(void)
 {
 	int n;
+	int c;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	int c = n % 10;
+	c = n % 10;
 	if (c > 5)
 	{
 		printf("Last digit of %d is %d and it is greater than 5\n", n, c);
@@ -35,5 +32,4 @@ int main(void)
 		printf("Last digit of %d is %d and it is less than 6 but not 0\n", n, c);
 	}
 	return (0);
-
 }
