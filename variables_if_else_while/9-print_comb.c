3e40129cdf40a598280c98ef02b770bf8c3a6e09
aliqulamaliyev <aliqulamaliyev@gmail.com>
2025-06-15 12:09:28 +0000

    completes the given task

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -10,15 +10,15 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 0; i < 9; i++)
+	for (i = 0; i <= 9; i++)
 	{
-			putchar(i + '0');
+		putchar(i + '0');
 
-			if (i != 8 || j != 9)
-			{
-				putchar(',');
-				putchar(' ');
-			}
+		if (i != 8 || j != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 
 	putchar('\n');
