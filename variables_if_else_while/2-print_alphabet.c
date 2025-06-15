c2d8c924235589ef046a93f8b631e0294cd44906
aliqulamaliyev <aliqulamaliyev@gmail.com>
2025-06-15 10:24:40 +0000

    completes the given task

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - entyr
+ * Description: This program generates a random number,
+ * checks if it is negative pososyive or zero
+ * It then prints a corresponding message based on the  digit.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char l;
+
+	for (l = 'a'; l <= 'z'; l++)
+	{
+		putchar(l);
+	}
+
+	putchar('\n');
+	return (0);
+}
