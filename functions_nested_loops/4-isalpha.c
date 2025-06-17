9cc9d273594cccbf56bfbef64b23093c5f6e0451
aliqulamaliyev <aliqulamaliyev@gmail.com>
2025-06-17 08:40:15 +0000

    complete task

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -7,9 +7,9 @@
  * Return: 1 if c is lowercase, 0 otherwise
  *
  */
-int _islower(int c)
+int _isalpha(int c)
 {
-	if (c >= 'a' && c <= 'z')
+	if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
 		return (1);
 	else
 		return (0);
