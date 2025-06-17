3daf93fee15b2e6040cbc415730ed998a1f37f88
aliqulamaliyev <aliqulamaliyev@gmail.com>
2025-06-17 07:42:28 +0000

    completes the given task

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -9,9 +9,9 @@ void print_alphabet(void)
 	char alphabet[] = "abcdefghijklmnopqrstuvwxyz\n";
 	int i = 0;
 
-    while (alphabet[i] != '\0')
-    {
-	    _putchar(alphabet[i]);
-	    i++;
-    }
+	while (alphabet[i] != '\0')
+	{
+		_putchar(alphabet[i]);
+		i++;
+	}
 }
