3091d88a34180dbd7369a2f799c3bb449bf7d401
aliqulamaliyev <aliqulamaliyev@gmail.com>
2025-06-19 10:36:09 +0000

    completes the task

--- functions_nested_loops/main.c
+++ functions_nested_loops/main.c
@@ -1,19 +1,11 @@
 #include "main.h"
 /**
- * main - Entry point
+ * main - ckeck the code
  *
  * Return: Always 0
  */
 int main(void)
 {
-	int r;
-	
-	print_last_digit(98);
-	print_last_digit(0);
-	r = print_last_digit(-1024);
-	_putchar('0' + r);
-	_putchar('\n');
-
+	jack_bauer();
 	return (0);
 }
-
