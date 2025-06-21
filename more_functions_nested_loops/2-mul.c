515033a203dee32f01adb0a5fc2a29a8672cc004
aliqulamaliyev <aliqulamaliyev@gmail.com>
2025-06-21 14:20:43 +0000

    completes the tasks

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - multpilies two integers
+ * @a: first integer 
+ * @b: second integer 
+ *
+ * Return: the result of multpilication
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
