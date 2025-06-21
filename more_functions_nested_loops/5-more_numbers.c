5c78388f52d7a276565c83e0af0283b299962fc5
aliqulamaliyev <aliqulamaliyev@gmail.com>
2025-06-21 16:16:00 +0000

    completes the tasks

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * more_numbers - prints the numbers from 0 to 14
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+	char digits[] = "01234567891011121314\n";
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		j = 0;
+		while (digits[j] != '\0')
+		{
+			_putchar(digits[j]);
+			j++;
+		}
+	}	
+}
--- more_functions_nested_loops/main.c
+++ more_functions_nested_loops/main.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-	print_most_numbers();
+	more_numbers();
 
 	return (0);
 }
