90a39743f46db2b8cbc27bb2bcc5ce04f85cbcb4
aliqulamaliyev <aliqulamaliey@gmail.com>
2025-06-22 19:30:57 +0000

    completes tasks

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * puts2 - prints every other character of a string, starting with the first
+ * puts2 - prints every other character of a string,
+ *         starting with the first character
  * @str: pointer to the string
  */
 void puts2(char *str)
