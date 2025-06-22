273930155695ff5b7c7162c0e46b51a16737a11e
aliqulamaliyev <aliqulamaliey@gmail.com>
2025-06-22 20:02:59 +0000

    completes tasks

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,8 +1,9 @@
+#include <limits.h>
 /**
  * _atoi - converts a string to an integer
  * @s: the string to convert
  *
- * Return: the integer representation, or 0 if no number
+ * Return: the integer converted from the string
  */
 int _atoi(char *s)
 {
@@ -11,12 +12,18 @@ int _atoi(char *s)
 
 	while (s[i] != '\0')
 	{
-
-		if (s[i] == '-')
+		if (s[i] == '-' && !started)
 			sign *= -1;
 		else if (s[i] >= '0' && s[i] <= '9')
 		{
-			result = result * 10 + (s[i] - '0');
+			int digit = s[i] - '0';
+
+			if (sign == 1 && (result > (INT_MAX - digit) / 10))
+				return INT_MAX;
+			if (sign == -1 && (result > (-(INT_MIN + digit)) / 10))
+				return INT_MIN;
+
+			result = result * 10 + digit;
 			started = 1;
 		}
 		else if (started)
