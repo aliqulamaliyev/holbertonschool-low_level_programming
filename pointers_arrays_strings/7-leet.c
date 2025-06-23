846736bd152067b8503643dffd0404a445f10598
aliqulamaliyev <aliqulamaliyev@gmail.com>
2025-06-23 09:34:40 +0000

    completes the c tasks

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+/**
+ * leet - encodes a string into 1337
+ * @s: input string
+ *
+ * Return: pointer to modified string
+ */
+char *leet(char *s)
+{
+	char letters[] = "aAeEoOtTlL";
+	char numbers[] = "4433007711";
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (s[i] == letters[j])
+			{
+				s[i] = numbers[j];
+				break;
+			}
+		}
+	}
+
+	return (s);
+}
