93705212f31e0b2f216013b5ce44f0720d3c2ef7
aliqulamaliyev <aliqulamaliyev@gmail.com>
2025-06-29 13:55:31 +0000

    completes the tasks

--- recursion/main.c
+++ recursion/main.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * main - check the code
@@ -7,7 +8,9 @@
  */
 int main(void)
 {
+	int n;
 
-	_print_rev_recursion("\nColton Walker");
+	n = _strlen_recursion("Corbin Coleman");
+	printf("%d\n", n);
 	return (0);
 }
