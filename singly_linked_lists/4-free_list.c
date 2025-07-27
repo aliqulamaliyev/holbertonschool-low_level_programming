9244ee82ab5b426b4be636814069913c095be0fc
aliqulamaliyev <aliqulamaliyev@gmail.com>
2025-07-27 17:43:42 +0000

    completes all c taska

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -1,6 +1,12 @@
 #include "lists.h"
 #include <stdlib.h>
-
+/**
+ * free_list - frees a linked list of type list_t
+ * @head: pointer to the head of the list to be freed
+ *
+ * Description: This function frees all the nodes of a linked list,
+ *              including the strings stored in each node.
+ */
 void free_list(list_t *head)
 {
 	list_t *tmp;
